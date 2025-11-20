ab850e79984ac7c793b479d48ee59721318e37a3
Thai Tran <thaitran@192.168.102.10>
2025-11-20 14:15:19 +0700

    fix loading

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import axiosClient from "../apis/axiosClient";
-import { DatePicker, Table } from "antd";
+import { DatePicker, Input, Table } from "antd";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import useAuth from "../hooks/useAuth";
@@ -12,19 +12,26 @@ import moment from "moment";
 function Account() {
   useAuth();
   const [date, setDate]: any = useState();
+  const [endDate, setEndDate]: any = useState();
+  const [id, setId]: any = useState();
   const [list, setList]: any[] = useState([]);
   const [totalBalance, setTotalBalance] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
   const getList = async () => {
+    setIsLoading(true);
     let response = await axiosClient.get("/accounts", {
       params: {
+        id: id,
         date: date,
+        endDate: endDate,
       },
     });
     setList(response.data.accounts);
+    setIsLoading(false);
   };
   useEffect(() => {
     getList();
-  }, [date]);
+  }, [date, endDate, id]);
 
   useEffect(() => {
     let total = list.reduce((sum: number, account: any) => sum + (account.balance || 0), 0);
@@ -32,14 +39,14 @@ function Account() {
   }, [list]);
 
   const columns = [
+    // {
+    //   title: "No.",
+    //   dataIndex: "",
+    //   key: "no",
+    //   render: (value: any, record: any, index: number) => index + 1,
+    // },
     {
-      title: "No.",
-      dataIndex: "",
-      key: "no",
-      render: (value: any, record: any, index: number) => index + 1,
-    },
-    {
-      title: "Id",
+      title: "ID",
       dataIndex: "account_id",
       key: "account_id",
     },
@@ -47,7 +54,13 @@ function Account() {
       title: "Balance",
       dataIndex: "balance",
       key: "balance",
-      render: (value: number) => value ? `$${(value / 100)?.toLocaleString("en-US", { minimumFractionDigits: 0 })}` : '',
+      render: (value: number) =>
+        value ? `$${(value / 100)?.toLocaleString("en-US", { minimumFractionDigits: 0 })}` : "",
+    },
+    {
+      title: "Vol",
+      dataIndex: "volume",
+      key: "volume",
     },
     {
       title: "Profit",
@@ -59,40 +72,66 @@ function Account() {
 
   return (
     <div className="container">
-      <div className="box">
-        <h2 className="title mt-3">Danh sách tài khoản</h2>
-        <div className="mt-2 mb-2 float-right">
-          <DatePicker
-            value={date ? dayjs(date) : null}
-            onChange={(value) => setDate(value ? value.format("YYYY-MM-DD") : null)}
-            format="YYYY-MM-DD"
-          />
+      {isLoading ? (
+        // loading spinner full page centered
+        <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-        <div className="clearfix">
-          <div className="float-left">Tổng số tiền: ${ (totalBalance / 100).toLocaleString("en-US", { minimumFractionDigits: 0 })} </div>
+      ) : (
+        <div className="box">
+          <h2 className="title mt-3">Danh sách tài khoản</h2>
+          <div className="clearfix">
+            <div className="float-left mr-3">
+              <b>Tổng số tiền:</b> ${(totalBalance / 100).toLocaleString("en-US", { minimumFractionDigits: 0 })}{" "}
+            </div>
+            <div className="float-left mr-3">
+              <b>Tổng số lãi:</b> $
+              {list
+                .reduce((sum: number, account: any) => sum + (account.total_profit || 0), 0)
+                .toLocaleString("en-US", { minimumFractionDigits: 0 })}{" "}
+            </div>
+            <div className="float-left mr-3">
+              <b>Tổng volums:</b>{" "}
+              {list
+                .reduce((sum: number, account: any) => sum + (account.volume || 0), 0)
+                .toLocaleString("en-US", { minimumFractionDigits: 0 })}{" "}
+              lô
+            </div>
+          </div>
+          <div className="row">
+            <div className="col col-md-4 mb-2 mt-2">
+              <Input
+                type="text"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                placeholder="Tìm theo ID"
+                style={{ width: "100%", marginRight: 10 }}
+              />
+            </div>
+            <div className="col col-md-4 mb-2 mt-2">
+              <DatePicker
+                value={date ? dayjs(date) : null}
+                onChange={(value) => setDate(value ? value.format("YYYY-MM-DD") : null)}
+                format="YYYY-MM-DD"
+                style={{ width: "100%" }}
+                placeholder="Chọn ngày bắt đầu"
+              />
+            </div>
+            <div className="col col-md-4 mb-2 mt-2">
+              <DatePicker
+                value={endDate ? dayjs(endDate) : null}
+                onChange={(value) => setEndDate(value ? value.format("YYYY-MM-DD") : null)}
+                format="YYYY-MM-DD"
+                style={{ width: "100%" }}
+                placeholder="Chọn ngày kết thúc"
+              />
+            </div>
+          </div>
+          <Table columns={columns} dataSource={list} rowKey="id" scroll={{ x: "max-content" }} />
         </div>
-        <Table columns={columns} dataSource={list} rowKey="id"  />
-        {/* <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Profit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map((user: any, index: number) => (
-              <tr key={user.id}>
-                <td>{index + 1}</td>
-                <td>{user.account_id}</td>
-                <td>{user.name}</td>
-                <td>{user.total_profit}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
-      </div>
+      )}
     </div>
   );
 }
